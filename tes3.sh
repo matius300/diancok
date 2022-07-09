bb6b26d97700a05b83113e416d9ca22228dbd113
matius300 <33297541+matius300@users.noreply.github.com>
2022-07-09 14:00:52 +0700

    Create tes3.sh

---
+++ tes3.sh
@@ -0,0 +1,37 @@
+wget -O ng.sh https://github.com/matius300/diancok/raw/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+
+
+function goto
+{
+    label=$1
+    cd 
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+: ngrok
+clear
+echo "Go to: https://dashboard.ngrok.com/signup"
+read -p "Paste Token Ngrok Nok Kene: " CRP
+./ngrok authtoken $CRP 
+
+clear
+echo ===== Azazil =====
+echo Proses Dulu Lan Sabar Rien 
+./ngrok tcp --region ap 4000 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=654321 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+clear
+echo "NoMachine: https://www.nomachine.com/download"
+echo Done! NoMachine Information:
+echo IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+echo User: user
+echo Passwd: 654321
+echo "VM can't connect? Restart Cloud Shell then Re-run script."
+seq 1 180000 | while read i; do echo -en "\r Running .     $i s /180000 s";sleep 0.1;echo -en "\r Running ..    $i s /180000 s";sleep 0.1;echo -en "\r Running ...   $i s /180000 s";sleep 0.1;echo -en "\r Running ....  $i s /180000 s";sleep 0.1;echo -en "\r Running ..... $i s /180000 s";sleep 0.1;echo -en "\r Running     . $i s /180000 s";sleep 0.1;echo -en "\r Running  .... $i s /180000 s";sleep 0.1;echo -en "\r Running   ... $i s /180000 s";sleep 0.1;echo -en "\r Running    .. $i s /180000 s";sleep 0.1;echo -en "\r Running     . $i s /180000 s";sleep 0.1;echo -en "\r Running .     $i s /180000 s";sleep 0.1;echo -en "\r Running ..    $i s /180000 s";sleep 0.1;echo -en "\r Running ...   $i s /180000 s";sleep 0.1;echo -en "\r Running ....  $i s /180000 s";sleep 0.1;echo -en "\r Running ..... $i s /180000 s";sleep 0.1;echo -en "\r Running     . $i s /180000 s";sleep 0.1;echo -en "\r Running    .. $i s /180000 s";sleep 0.1;echo -en "\r Running   ... $i s /180000 s";sleep 0.1;echo -en "\r Running  .... $i s /180000 s";sleep 0.1;echo -en "\r Running ..... $i s /180000 s";sleep 0.1;echo -en "\r Running .     $i s /180000 s";sleep 0.1;echo -en "\r Running ..    $i s /180000 s";sleep 0.1;echo -en "\r Running ...   $i s /180000 s";sleep 0.1;echo -en "\r Running ....  $i s /180000 s";sleep 0.1;echo -en "\r Running ..... $i s /180000 s";sleep 0.1;echo -en "\r Running     . $i s /180000 s";sleep 0.1;echo -en "\r Running    .. $i s /180000 s";sleep 0.1;echo -en "\r Running   ... $i s /180000 s";sleep 0.1;echo -en "\r Running  .... $i s /180000 s";sleep 0.1;echo -en "\r Running ..... $i s /180000 s";sleep 0.1;echo -en "\r Running .     $i s /180000 s";sleep 0.1;echo -en "\r Running ..    $i s /180000 s";sleep 0.1;echo -en "\r Running ...   $i s /180000 s";sleep 0.1;echo -en "\r Running ....  $i s /180000 s";sleep 0.1;echo -en "\r Running ..... $i s /180000 s";sleep 0.1;echo -en "\r Running     . $i s /180000 s";sleep 0.1;echo -en "\r Running    .. $i s /180000 s";sleep 0.1;echo -en "\r Running   ... $i s /180000 s";sleep 0.1;echo -en "\r Running  .... $i s /180000 s";sleep 0.1;echo -en "\r Running ..... $i s /180000 s";sleep 0.1;echo -en "\r Running .     $i s /180000 s";sleep 0.1;echo -en "\r Running ..    $i s /180000 s";sleep 0.1;echo -en "\r Running ...   $i s /180000 s";sleep 0.1;echo -en "\r Running ....  $i s /180000 s";sleep 0.1;echo -en "\r Running ..... $i s /180000 s";sleep 0.1;echo -en "\r Running     . $i s /180000 s";sleep 0.1;echo -en "\r Running    .. $i s /180000 s";sleep 0.1;echo -en "\r Running   ... $i s /180000 s";sleep 0.1;echo -en "\r Running  .... $i s /180000 s";sleep 0.1; done
